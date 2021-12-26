d7b644ee8130e4a770603892ddfba50046efe20a
A <13673553+lewisha@users.noreply.github.com>
2021-12-26 11:21:32 -0500

    Add files via upload

---
+++ hw2_PartTwo.js
@@ -0,0 +1,7 @@
+// Question Two
+const string =
+  "Perhaps The Easiest-To-Understand Case For Reduce Is To Return The Sum Of All The Elements In An Array";
+console.log(string);
+
+console.log(string.split("").join().toLowerCase());
+// is remove useful??
