47fdeb46310306cb1205ff2fe55610aa2f5305c8
A <13673553+lewisha@users.noreply.github.com>
2022-01-05 19:37:12 -0500

    Update hw2_PartTwo.js

--- hw2_PartTwo.js
+++ hw2_PartTwo.js
@@ -3,5 +3,4 @@ const string =
   "Perhaps The Easiest-To-Understand Case For Reduce Is To Return The Sum Of All The Elements In An Array";
 console.log(string);
 
-console.log(string.split("").join().toLowerCase());
-// is remove useful??
+console.log(string.trim().toLowerCase().split(/[ ]+/).join(' '));
